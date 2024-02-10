c21ab7e403e5650eb485c5a83ca9ef5fa6509453
Jake Overall <joverall22@gmail.com>
2024-02-10 09:53:53 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let tax = 1.82
+let lobstersInTank = 18
+let lobsterEmoji = '🦞'
+let restaurant = {
+  name: "Boca Coast Ristorante",
+  street: "594 Lakeshore Street",
+  city: 'Torrington, CT 06790',
+  michelinStars: 2.5
+}
+
+let menu = [
+  { name: 'Lobster Bisque 🦞', price: 18.75, glutenFree: false },
+  { name: 'Seared Halibut 🐠', price: 22.89, glutenFree: true },
+  { name: 'Clam Chowder Bread Bowl 🦪', price: 21.89, glutenFree: false },
+  { name: 'Chicken Nuggets 🍗', price: 11, glutenFree: false },
+  { name: 'Ceasar Salad 🥗', price: 18, glutenFree: true },
+]
